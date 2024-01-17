3ea6898aa94e335e3c6af4dfa768b0d0a6d75f6e
charlie-snow <victo@victoriano.none>
2024-01-17 15:17:44 +0100

    registro funcionando V0.1

---
+++ travel_experiences/env.js
@@ -0,0 +1,31 @@
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+// const {
+//   PORT,
+//   HOST_DB,
+//   USER_DB,
+//   PASSWORD_DB,
+//   PORT_DB,
+//   NAME_DB,
+//   TOKEN_SECRET,
+//   SMTP_HOST,
+//   SMTP_PORT,
+//   SMTP_USER,
+//   SMTP_PASS,
+// } = process.env;
+
+// export {
+//   PORT,
+//   HOST_DB,
+//   USER_DB,
+//   PASSWORD_DB,
+//   PORT_DB,
+//   NAME_DB,
+//   TOKEN_SECRET,
+//   SMTP_HOST,
+//   SMTP_PORT,
+//   SMTP_USER,
+//   SMTP_PASS,
+// };
